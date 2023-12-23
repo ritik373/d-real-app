b641aa027e5adcd9a2521a55df72b11d94f1ccb4
ritik <ritik@codewithritik.com>
2023-12-23 13:10:05 +0530

    d Real notify

---
+++ index.js
@@ -0,0 +1,79 @@
+const express=require('express');
+const bodyParser=require('body-parser');
+const cors=require('cors');
+const {ethers}=require('ethers');
+const app=express();
+const mysql = require('mysql2');
+// const fetch=require('node-fetch');
+// const socketIO=require('socket.io');
+const { Alchemy, Network, Utils, Contract } = require("alchemy-sdk");
+const  { ETH_USDC_PAIR_ABI_STRING, UNISWAP_V2_PAIR_CONTRACT_ADDRESS }=require('./abi.js');
+
+
+app.use(express.json());
+app.use(cors());
+app.use(bodyParser.json());
+
+
+app.post('/webhook', (req, res) => {
+    // Process the incoming webhook payload here
+    //console.log('Webhook payload:', req.body.event.activity);
+    console.log(req.body);
+
+    // let userAddress = req.body.event.activity[0].toAddress;
+    // let userMessage = `You received ${req.body.event.activity[0].value} ${req.body.event.activity[0].asset} from ${req.body.event.activity[0].fromAddress}`;
+
+    // console.log(userAddress);
+    // console.log(userMessage);
+
+    // Store the data in the database
+    // const query = 'INSERT INTO notification (userAddress, notification_msg, isRead) VALUES (?,?,?)';
+    // const values = [userAddress, userMessage, false];
+
+    // connection.query(query, values, (err, results) => {
+    //     if (err) {
+    //         console.error('Error inserting data into MySQL:', err);
+    //         res.status(500).json({ error: 'Internal Server Error' });
+    //         return;
+    //     }
+    //     console.log("stored in DB")
+    // });
+
+    // Send a response if needed
+    res.json({ status: 'Received the webhook payload' });
+});
+
+
+app.get('/notifications', (req, res) => {
+    const userAddress = req.query.userAddress;
+
+    console.log(userAddress);
+    // Query to fetch notifications for a given userId
+    const query = 'SELECT * FROM notification WHERE userAddress = ?';
+
+    connection.query(query, [userAddress], (err, results) => {
+        if (err) {
+            console.error('Error fetching notifications from MySQL:', err);
+            res.status(500).json({ error: 'Internal Server Error' });
+            return;
+        }
+
+        let finalResult = {
+            status: true,
+            result : results
+        }
+        console.log(results);
+        res.json(finalResult);
+    });
+});
+
+
+
+
+
+
+
+
+app.listen(3000,()=>{
+    console.log("listening on port 3000");
+})
